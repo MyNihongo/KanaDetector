1e23ad6e5d7f2875e56279fd35bd10f956902a55
My Nihongo <65362546+my-nihongo@users.noreply.github.com>
2021-03-09 22:58:24 +0200

    Add files via upload

---
+++ src/Extensions/CharExtensions.cs
@@ -0,0 +1,44 @@
+﻿/*
+* Copyright © 2021 MyNihongo
+*/
+
+using System;
+using static MyNihongo.KanaDetector.Resources.Constants;
+
+namespace MyNihongo.KanaDetector.Extensions
+{
+	public static class CharExtensions
+	{
+		public static bool IsHiragana(this char @this) =>
+			IsHiragana(Convert.ToInt32(@this));
+
+		public static bool IsKatakana(this char @this) =>
+			IsKatakana(Convert.ToInt32(@this));
+
+		public static bool IsKana(this char @this)
+		{
+			var @int = Convert.ToInt32(@this);
+			return IsHiragana(@int) || IsKatakana(@int);
+		}
+
+		public static bool IsKanji(this char @this) =>
+			IsKanji(Convert.ToInt32(@this));
+
+		public static bool IsKanaOrKanji(this char @this)
+		{
+			var @int = Convert.ToInt32(@this);
+			return IsHiragana(@int) || IsKatakana(@int) || IsKanji(@int);
+		}
+
+		private static bool IsHiragana(int number) =>
+			number >= Kana.HiraganaStart && number <= Kana.HiraganaEnd;
+
+		private static bool IsKatakana(int number) =>
+			number >= Kana.KatakanaStart && number <= Kana.KatakanaEnd;
+
+		private static bool IsKanji(int number) =>
+			number >= Kanji.Start && number <= Kanji.End ||
+			number >= Kanji.RareStart && number <= Kanji.RareEnd ||
+			number == Kanji.IterationMark;
+	}
+}
---
+++ src/Resources/Constants.cs
@@ -0,0 +1,27 @@
+﻿/*
+* Copyright © 2021 MyNihongo
+*/
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MyNihongo.KanaDetector.Tests")]
+namespace MyNihongo.KanaDetector.Resources
+{
+	internal static class Constants
+	{
+		internal static class Kana
+		{
+			internal const int HiraganaStart = 0x3041; // // ぁ
+			internal const int HiraganaEnd = 0x3096; // ゖ
+			internal const int KatakanaStart = 0x30a1; // ァ
+			internal const int KatakanaEnd = 0x30fc; // ー
+		}
+
+		internal static class Kanji
+		{
+			internal const int Start = 0x4e00; // 一
+			internal const int End = 0x9faf; // 龯
+			internal const int RareStart = 0x3400; // 㐀
+			internal const int RareEnd = 0x4db5; // 䶵
+			internal const int IterationMark = 0x3005; // 々
+		}
+	}
+}
